005d3c0211f4fff2379708391e9fff56f51c9818
Nathan <Nathan>
2016-09-26 14:04:31 -0400

    Initial commit

---
+++ app/src/androidTest/java/com/whitfield/nathan/csis/FacultyActivity.java
@@ -0,0 +1,16 @@
+package com.whitfield.nathan.csis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+/**
+ * Created by Nathan on 9/26/2016.
+ */
+public class FacultyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faculty);
+    }
+}
